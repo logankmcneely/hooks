ab0aa4c613c18de6f49b3a38905f27e5ec8ac7e1
Logan McNeely <logan.k.mcneely@gmail.com>
2020-06-22 11:55:27 -0500

    Added error modal

--- src/components/Ingredients/Ingredients.js
+++ src/components/Ingredients/Ingredients.js
@@ -3,11 +3,13 @@ import React, { useState, useCallback } from 'react';
 import IngredientForm from './IngredientForm';
 import IngredientList from './IngredientList';
 import Search from './Search';
+import ErrorModal from '../UI/ErrorModal';
 
 const Ingredients = () => {
 
   const [userIngredients, setUserIngredients] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState();
 
   const addIngredientHandler = ingredient => {
     setIsLoading(true);
@@ -40,15 +42,23 @@ const Ingredients = () => {
       setUserIngredients(prevIngredients =>
         prevIngredients.filter((ingredient) => ingredient.id !== id)
       );
-    })
+    }).catch(error => {
+      setError('Something went wrong!');
+      setIsLoading(false);
+    });
   };
 
   const filteredIngredientsHandler = useCallback(filteredIngredients => {
     setUserIngredients(filteredIngredients)
   }, []);
 
+  const clearError = () => {
+    setError(null);
+  };
+
   return (
     <div className="App">
+      {error && <ErrorModal onClose={clearError}>{error}</ErrorModal>}
       <IngredientForm
         onAddIngredient={addIngredientHandler}
         loading={isLoading} />
