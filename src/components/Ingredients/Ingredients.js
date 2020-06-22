a17e02241357af7500e0dd506cae164ab39d5b89
Logan McNeely <logan.k.mcneely@gmail.com>
2020-06-22 11:33:11 -0500

    Updated search functionality/added timer

--- src/components/Ingredients/Ingredients.js
+++ src/components/Ingredients/Ingredients.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useCallback } from 'react';
 
 import IngredientForm from './IngredientForm';
 import IngredientList from './IngredientList';
@@ -7,23 +7,6 @@ import Search from './Search';
 const Ingredients = () => {
 
   const [userIngredients, setUserIngredients] = useState([]);
-
-  useEffect(() => {
-    fetch('https://react-hooks-bb805.firebaseio.com/ingredients.json')
-      .then(response => response.json())
-      .then(responseData => {
-        console.log(responseData);
-        const loadedIngredients = [];
-        for (const key in responseData) {
-          loadedIngredients.push({
-            id: key,
-            title: responseData[key].title,
-            amount: responseData[key].amount
-          });
-        }
-        setUserIngredients(loadedIngredients);
-      });
-  }, []);
 
   const addIngredientHandler = ingredient => {
     fetch('https://react-hooks-bb805.firebaseio.com/ingredients.json', {
@@ -51,13 +34,17 @@ const Ingredients = () => {
     );
   };
 
+  const filteredIngredientsHandler = useCallback(filteredIngredients => {
+    setUserIngredients(filteredIngredients)
+  }, []);
+
   return (
     <div className="App">
       <IngredientForm
         onAddIngredient={addIngredientHandler} />
 
       <section>
-        <Search />
+        <Search onLoadIngredients={filteredIngredientsHandler}/>
         <IngredientList
           ingredients={userIngredients}
           onRemoveItem={removeIngredientHandler} />
